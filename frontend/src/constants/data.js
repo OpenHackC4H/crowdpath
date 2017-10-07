e0b8d052715098f1054b47704c64b0c39f6860f2
Herman Jonsson <hermanjonsson@Hermans-MacBook-Air.local>
2017-10-07 17:05:48 +0200

    Adding some more sites to the data file

--- frontend/src/constants/data.js
+++ frontend/src/constants/data.js
@@ -9,25 +9,25 @@ const payload = [
         tags: []
     }, {
         id:1,
-        title: "Title 1",
-        url:"fakeurl 1",
-        text:"This is a dummy text too",
+        title: "Applying for asylum",
+        url:"https://www.migrationsverket.se/English/Private-individuals/Protection-and-asylum-in-Sweden/Applying-for-asylum.html",
+        text:"Article from the immigration office about the asylum seeking process.",
         upVotes: 40,
         downVotes:53,
         tags:[]
     },{
         id:2,
-        title: "Title 2",
-        url:"fakeurl 2",
-        text:"Useless text",
+        title: "Short overview of the asylum process",
+        url:"http://www.asylumineurope.org/reports/country/sweden/short-overview-asylum-procedure",
+        text:"Overview article about the asylum process in Sweden",
         upVotes: 200,
         downVotes:17,
         tags:[]
     },{
         id:3,
-        title: "Title 3",
-        url:"fakeurl 3",
-        text:"Useless text too",
+        title: "This is IM",
+        url:"https://manniskohjalp.se/en/im",
+        text:"IM is a development organisation fighting and exposing poverty and exclusion. The organisation was founded in 1938 and is working in thirteen countries worldwide.",
         upVotes: 40,
         downVotes:501,
         tags:[]
