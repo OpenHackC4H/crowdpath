ff9d041dca62508dd9d5d314529b3ba57390a462
joseph_ctu <sephxd1234@gmail.com>
2017-10-07 13:13:57 +0200

    Responsive header. Added constants

---
+++ frontend/src/constants/data.js
@@ -0,0 +1,17 @@
+const payload = [
+    {
+        id:0,
+        title: "Title 0",
+        url:"fakeurl 0",
+        up: 100,
+        down:33
+    }, {
+        id:1,
+        title: "Title 1",
+        url:"fakeurl 1",
+        up: 40,
+        down:53
+    }
+]
+
+export const payload;
