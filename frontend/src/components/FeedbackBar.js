0f51c4ca3121567a0c1ac47ab4f88ab3566bd5e9
joseph_ctu <sephxd1234@gmail.com>
2017-10-08 01:15:30 +0200

    Added voting functionality

--- frontend/src/components/FeedbackBar.js
+++ frontend/src/components/FeedbackBar.js
@@ -1,21 +1,82 @@
-import React from "react";
-import { Button, ButtonGroup, Container, Row, Col } from "reactstrap";
+import React, { Component } from "react";
+import { Button, Container, Row, Col } from "reactstrap";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import "./FeedbackBar.css";
 
-class FeedbackBar extends React.Component {
+import { upVoteArticle, downVoteArticle } from "../actions";
+
+class FeedbackBar extends Component {
+  functionToUpVote(id, upVotes) {
+    this.props.actions.upVoteArticle(id, upVotes);
+  }
+
+  functionToDownVote(id, downVotes) {
+    this.props.actions.downVoteArticle(id, downVotes);
+  }
+
   render() {
+    const currentId = this.props.currentArticle.id;
+    const [thisArticle] = this.props.articles.filter(
+      article => (article.id === currentId ? article : null)
+    );
     return (
-      <Container>
-        <Button className="up-button" color="success">
-          Vote Up
-        </Button>{" "}
-        <Button className="down-button" color="danger">
-          Vote Down
-        </Button>{" "}
-        <Button className="report-button">Report</Button>
+      <Container style={{ marginTop: "25px", width: "auto" }}>
+        <Row>
+          <Col xs="12" style={{ textAlign: "center" }}>
+            <Button
+              style={buttonStyle}
+              className="up-button"
+              color="success"
+              onClick={this.functionToUpVote.bind(
+                this,
+                currentId,
+                thisArticle.upVotes
+              )}
+            >
+              {thisArticle.upVotes} Vote Up
+            </Button>{" "}
+            <Button
+              className="down-button"
+              color="danger"
+              style={buttonStyle}
+              onClick={this.functionToDownVote.bind(
+                this,
+                currentId,
+                thisArticle.downVotes
+              )}
+            >
+              {thisArticle.downVotes} Vote Down
+            </Button>{" "}
+            <Button className="report-button">Report</Button>
+          </Col>
+        </Row>
       </Container>
     );
   }
 }
 
-export default FeedbackBar;
+function mapStateToProps(state) {
+  return {
+    articles: state.articles.articles
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(
+      {
+        upVoteArticle,
+        downVoteArticle
+      },
+      dispatch
+    )
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FeedbackBar);
+
+const buttonStyle = {
+  width: "150px",
+  marginRight: "20px"
+};
