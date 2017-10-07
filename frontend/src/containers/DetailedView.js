b7a38b75e9b38e77735561c20ddfea55b370d271
joseph_ctu <sephxd1234@gmail.com>
2017-10-07 15:55:57 +0200

    Removed css and logo. Added Payload data and added iframe src

--- frontend/src/containers/DetailedView.js
+++ frontend/src/containers/DetailedView.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import { Container, Col, Row } from "reactstrap";
-import logo from "../logo.svg";
-import "./App.css";
+import Payload from "../constants/data";
 
 class DetailedView extends Component {
   render() {
+    const [iframeURL] = Payload.filter(
+      article => article.id === Number(this.props.match.params.id)
+    );
+
     return (
       <div className="DetailedView">
-
         <Container>
           <Row>
-            <Col><iframe src="URL" height="500" width="900" margin= "auto"/></Col>
+            <Col>
+              <iframe
+                title="Detailed View"
+                src={iframeURL.url}
+                height="500"
+                width="900"
+                margin="auto"
+              />
+            </Col>
           </Row>
         </Container>
       </div>
