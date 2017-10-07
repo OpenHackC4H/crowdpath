e94d81b9a11eb0828e0fbbbab417e11f0ebbbea9
joseph_ctu <sephxd1234@gmail.com>
2017-10-08 01:15:59 +0200

    added various types

--- frontend/src/constants/types.js
+++ frontend/src/constants/types.js
@@ -8,5 +8,4 @@ export const UPVOTE = "up_votes_an_article";
 export const DOWNVOTE = "down_votes_an_article";
 export const READ = "user_reads_an_article";
 
-export const GET_UPVOTES = "get_up_votes";
-export const GET_DOWNVOTES = "get_down_votes";
+export const UPDATE_VOTES = "update_votes";
