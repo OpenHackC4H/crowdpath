251bb26cf92d2adf857ce8d1c9b5b9e23f8aaa63
joseph_ctu <sephxd1234@gmail.com>
2017-10-07 19:14:01 +0200

    Added action types

--- frontend/src/constants/types.js
+++ frontend/src/constants/types.js
@@ -1,4 +1,9 @@
-export const SEARCH = "SEARCH_information";
+export const SHOW_ALL = "show_all_articles";
+
+export const FETCH = "fetching articles";
+export const FETCH_FAILED = "failed_to_fetch";
+
+export const SEARCH = "search_information";
 export const UPVOTE = "up_votes_an_article";
 export const DOWNVOTE = "down_votes_an_article";
 export const READ = "user_reads_an_article";
