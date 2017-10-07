d9f70c234137045007182c05821b6a409cd0cc92
joseph_ctu <sephxd1234@gmail.com>
2017-10-08 01:20:36 +0200

    SearchBar is now a class component

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { InputGroup, InputGroupButton, Input } from "reactstrap";
 import "./SearchBar.css";
 
-const SearchBar = props => {
-  return (
-    <div>
-      <InputGroup>
-        <Input placeholder="What do you need?" />
-        <InputGroupButton color="primary">Find it!</InputGroupButton>
-      </InputGroup>
-    </div>
-  );
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: ""
+    };
+  }
+
+  onInputChange(searchString) {
+    this.setState({searchString});
+  }
+
+  render() {
+    return (
+      <div>
+        <InputGroup>
+          <Input
+            type="text"
+            value={this.state.searchString}
+            onChange={e => this.onInputChange(e.target.value)}
+            placeholder="What do you need?"
+          />
+          <InputGroupButton color="primary">Find it!</InputGroupButton>
+        </InputGroup>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
