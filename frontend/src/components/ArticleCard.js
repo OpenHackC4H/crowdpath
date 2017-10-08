a803580c75ae75e37d11346dec270d28f5b38228
joseph_ctu <sephxd1234@gmail.com>
2017-10-08 10:34:06 +0200

    Added tags under the title

--- frontend/src/components/ArticleCard.js
+++ frontend/src/components/ArticleCard.js
@@ -4,29 +4,49 @@ import { NavLink as RRNavLink } from "react-router-dom";
 
 const ArticleCard = ({
   props,
-  article: { id, title, url, text, upVotes, downVotes }
+  article: { id, title, url, text, upVotes, downVotes, tags }
 }) => {
   const articleURL = `/DetailedView/${id}`;
+
+  const tagList = tags.map(
+    (tag, i) =>
+      i < 5 ? (
+        <span key={i}>
+          {i === 0 ? "" : ", "}
+          {tag}
+        </span>
+      ) : null
+  );
+  console.log(tags);
   return (
     <Row>
       <Col>
         <ListGroupItemHeading className="lead">
           <RRNavLink to={articleURL}>{title}</RRNavLink>
         </ListGroupItemHeading>
+        <ListGroupItemText style={{ color: "gray" }}>
+          tags: {tagList}
+        </ListGroupItemText>
       </Col>
       <Col>
         <Row>
           <Col>
             <ListGroupItemText>
               <span className="lead" style={upVotesStyle}>
-                {upVotes > 1000 ? `${Math.ceil(upVotes / 100)/10}k` : upVotes} ↑
+                {upVotes > 1000
+                  ? `${Math.ceil(upVotes / 100) / 10}k`
+                  : upVotes}{" "}
+                ↑
               </span>
             </ListGroupItemText>
           </Col>
           <Col>
             <ListGroupItemText>
               <span className="lead" style={downVotesStyle}>
-                {downVotes > 1000 ? `${Math.ceil(downVotes / 100)/10}k` : downVotes} ↓
+                {downVotes > 1000
+                  ? `${Math.ceil(downVotes / 100) / 10}k`
+                  : downVotes}{" "}
+                ↓
               </span>
             </ListGroupItemText>
           </Col>
