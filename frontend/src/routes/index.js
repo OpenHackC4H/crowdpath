9e5fa10f900f5e8c8a6976606eeeb7e48c679dd9
joseph_ctu <sephxd1234@gmail.com>
2017-10-08 04:15:58 +0200

    Added footer to routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router";
 import App from "../containers/App";
 import DetailedView from "../containers/DetailedView";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const routes = (
   <div>
@@ -12,6 +13,7 @@ const routes = (
       <Route exact path="/" component={App} />
       <Route path="/DetailedView/:id" component={DetailedView} />
     </Switch>
+    <Footer />
   </div>
 );
 
